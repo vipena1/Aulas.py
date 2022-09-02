55a079e14e698748191d9e8a043e11061d25c732
vipena1 <viniciuspena81@gmail.com.br>
2022-09-01 22:59:03 -0300

    Exercicio de pratica

---
+++ exercicio03.py
@@ -0,0 +1,9 @@
+# Escreva um programa que calcule a soma de todos os números pares entre 1 e 20.
+
+soma = 0
+
+for num in range(1, 21):
+    if num% 2 == 0:
+        soma = soma + num
+
+print(f'A soma dos números pares entre 1 e 20 é: {soma}')
