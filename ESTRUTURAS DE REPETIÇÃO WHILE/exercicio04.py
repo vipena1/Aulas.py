8db2c66ea90b0e717507f3cfa40c1df224f2551e
vipena1 <viniciuspena81@gmail.com.br>
2022-09-13 21:21:54 -0300

    Exercicios de estrutura de repetição

---
+++ ESTRUTURAS DE REPETIÇÃO WHILE/exercicio04.py
@@ -0,0 +1,8 @@
+"""Escreva um algoritmo que solicite dois números e devolva quantos pares e
+ímpares há entre esses dois números. Exemplo: entre 7 e 10 há 2 números
+pares e 2 números ímpares
+"""
+
+numUm = int(input("Digite o primeiro número: "))
+numDois = int(input("Digite o segundo número: "))
+
