f12de7da57bac000143bc184e8bea70b4a570004
vipena1 <viniciuspena81@gmail.com.br>
2022-09-15 21:56:26 -0300

    Realização dos exercicio lista 8

--- ESTRUTURAS DE REPETIÇÃO WHILE/exercicio03.py
+++ ESTRUTURAS DE REPETIÇÃO WHILE/exercicio03.py
@@ -1,14 +1,13 @@
 """Escreva um algoritmo para ler 10 números. Todos os números lidos com valor inferior a 40 devem ser somados.
 Escreva o valor final da soma efetuada. """
 
-num = int(input("Digite um número: "))
-
 cont = 1
-soma = num
+soma = 0
 
-while num < 40 and cont < 10:
-    soma = soma + num
+while cont <= 10:
+    num = int(input("Digite um número: "))
+    if num < 40:
+        soma = soma + num
     cont = cont + 1
-    num = int(input("Digite outro número: "))
 
 print(f"A soma dos valores é: {soma}")
