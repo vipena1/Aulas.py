373d4f29d38064f58ab5acb09fbddd8b63345841
vipena1 <viniciuspena81@gmail.com.br>
2022-09-22 22:21:29 -0300

    Exercicio "WHILE"

---
+++ ESTRUTURAS DE REPETIÇÃO WHILE/aula03.py
@@ -0,0 +1,14 @@
+cont = 1
+
+while (cont <= 3):
+
+    num = int(input("Digite um número para saber se é impar ou par: "))
+
+    if (num % 2 == 0):
+        print("Par")
+
+    else:
+        print("Impar")
+
+    cont = cont + 1
+
